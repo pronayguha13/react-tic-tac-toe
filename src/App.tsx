803c0f2ce4e86242b123daa0987d86b9ceff3240
Pronay Guha <pronayguha13@gmail.com>
2025-07-25 18:32:06 +0530

    Added lint-staged configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+/* eslint-disable no-alert */
+import { useState } from "react";
+
 import Grid from "./components/Grid";
+import "./app.css";
 
 function App() {
+  const [firstPlayerScore, setFirstPlayerScore] = useState<number>(0);
+  const [secondPlayerScore, setSecondPlayerScore] = useState<number>(0);
+
+  const updateScore = (winner: string) => {
+    if (winner === "firstPlayer") {
+      setFirstPlayerScore(prev => prev + 1);
+      alert("First Player won!");
+    }
+    else {
+      setSecondPlayerScore(prev => prev + 1);
+      alert("Second Player won!");
+    }
+  };
+
   return (
-    <div>
-      <Grid />
+    <div className="container">
+      <Grid updateScore={updateScore} />
+
+      <div className="scoreboard">
+
+        <h4>
+          First Player Score:
+          {firstPlayerScore}
+        </h4>
+        <h4>
+          Second Player Score:
+          {secondPlayerScore}
+        </h4>
+      </div>
     </div>
   );
 }
--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,91 +1,117 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Square from "../Square";
 import "./style.css";
 
-function Grid() {
+function Grid({ updateScore }: { updateScore: (winner: string) => void }) {
   const [isFirstPlayerTurn, setIsFirstPlayerTurn] = useState<boolean>(true);
-  const [playerMoves, setPlayerMoves] = useState<Map<string, Array<number>>>(
-    () => new Map([
-      ["firstPlayer", []],
-      ["secondPlayer", []],
-    ]),
-  );
+  const [board, setBoard] = useState<Array<Array<string>>>(Array.from({ length: 3 }).fill(null).map(() => Array.from({ length: 3 }).fill("")) as Array<Array<string>>);
+  const [winner, setWinner] = useState<string | null>(null);
+  const moveCount = useRef<number>(0);
 
-  const updatePlayerMove = (player: string, cellNumber: number) => {
-    const updatedPlayerMoves = new Map(playerMoves);
-    const existingMoves = updatedPlayerMoves.get(player) || [];
+  const handleCellClick = (cellIndex: number, rowIndex: number) => {
+    const playerMark = isFirstPlayerTurn ? "X" : "O";
+    const tempBoard = board.map(row => [...row]);
+    if (tempBoard[rowIndex][cellIndex].length)
+      return;
 
-    updatedPlayerMoves.set(player, [...existingMoves, cellNumber]);
-    setPlayerMoves(updatedPlayerMoves);
+    tempBoard[rowIndex][cellIndex] = playerMark;
+    setBoard(tempBoard);
     setIsFirstPlayerTurn(prev => !prev);
-  };
-  /**
-   * @description Function to handle user move by clicking on any Square component
-   * @param {number} cellNumber index of the cell where the user has clicked
-   */
-  const handleClick = (cellNumber: number) => {
-    updatePlayerMove(
-      isFirstPlayerTurn ? "firstPlayer" : "secondPlayer",
-      cellNumber,
-    );
-  };
-  /**
-   * @description Function to get whether a cell is selected or not
-   * @param cellIndex index of the cell
-   * @returns {boolean} whether the cell is selected or not
-   */
-  const getIsSelected = (cellIndex: number): boolean => {
-    const firstPlayerMoves: Array<number>
-      = playerMoves.get("firstPlayer") || [];
-
-    const secondPlayerMoves: Array<number>
-      = playerMoves.get("secondPlayer") || [];
-
-    return (
-      firstPlayerMoves.includes(cellIndex)
-      || secondPlayerMoves.includes(cellIndex)
-    );
-  };
-  /**
-   * @description Function to decide whether the cell is selected or not and by whom
-   * @param cellIndex index of the cell selected
-   * @returns {string | null} name of the selected player or null
-   */
-  const getPlayerSelection = (cellIndex: number): string | null => {
-    const firstPlayerMoves: Array<number>
-      = playerMoves.get("firstPlayer") || [];
-
-    const secondPlayerMoves: Array<number>
-      = playerMoves.get("secondPlayer") || [];
-
-    if (firstPlayerMoves.includes(cellIndex))
-      return "firstPlayer";
-    if (secondPlayerMoves.includes(cellIndex))
-      return "secondPlayer";
-
-    return null;
+    moveCount.current = moveCount.current + 1;
   };
 
-  // const findPattern = () => {
-  //   const firstPlayerMoves = playerMoves.get("firstPlayer") || [];
-  //   const secondPlayerMoves = playerMoves.get("secondPlayer") || [];
-  //   // if there are less than three moves then move on
-  //   // if (isFirstPlayerTurn) {
+  const allEqual = (sequence: string[]): boolean => {
+    // ! Check whether all the elements have length or not
+    const isAnyEmpty = sequence.some(element => !element.length);
 
-  //   // }
-  // };
+    if (isAnyEmpty)
+      return false;
+
+    const isAllEqual = sequence.every(val => val === sequence[0]);
+    const winnerPlayer = !isAllEqual ? null : sequence[0] === "X" ? "firstPlayer" : "secondPlayer";
+    if (winnerPlayer) {
+      setWinner(winnerPlayer);
+    }
+    return isAllEqual;
+  };
+
+  const checkWinner = () => {
+    let isWinnerFound = false;
+    // ! First check the rows
+    board.forEach((row: string[]) => {
+      const isEqual = allEqual(row);
+
+      if (isEqual) {
+        isWinnerFound = true;
+      }
+    });
+
+    // ! Second check the columns
+    if (!isWinnerFound) {
+      for (let i = 0; i < board.length; i++) {
+        const column: string[] = [];
+        board.forEach((row) => {
+          column.push(row[i]);
+        });
+
+        const isAllEqual = allEqual(column);
+        console.log("🚀 ~ checkWinner ~ isAllEqual:", isAllEqual);
+
+        if (isAllEqual) {
+          isWinnerFound = true;
+          return;
+        }
+      }
+    }
+    // ! Third check the two diagonals
+    for (let i = 0; i < 2; i++) {
+      const firstDiagonal = [board[0][0], board[1][1], board[2][2]];
+      const secondDiagonal = [board[0][2], board[1][1], board[2][0]];
+
+      if (allEqual(firstDiagonal)) {
+        isWinnerFound = true;
+        break;
+      }
+      else if (allEqual(secondDiagonal)) {
+        isWinnerFound = true;
+      }
+    }
+  };
+
+  const resetGame = () => {
+    setBoard(Array.from({ length: 3 }).fill(null).map(() => Array.from({ length: 3 }).fill("")) as Array<Array<string>>);
+    setIsFirstPlayerTurn(true);
+    setWinner(null);
+    moveCount.current = 0;
+  };
+
+  useEffect(() => {
+    if (winner) {
+      updateScore(winner);
+      resetGame();
+    }
+
+    if (moveCount.current === 9)
+      resetGame();
+  }, [winner]);
+
+  useEffect(() => {
+    if (moveCount.current >= 5) {
+      checkWinner();
+    }
+  }, [moveCount.current]);
+
+  const paintBoard = () =>
+    board.map((row, rowIndex) => {
+      return row.map((cell, cellIndex) => (
+        <Square key={cellIndex} content={cell} handleClick={() => handleCellClick(cellIndex, rowIndex)} />
+      ));
+    });
 
   return (
     <div className="grid">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((el, index) => (
-        <Square
-          key={el}
-          handleClick={() => handleClick(index)}
-          isSelected={getIsSelected(index)}
-          player={getPlayerSelection(index)}
-        />
-      ))}
+      {paintBoard()}
     </div>
   );
 }
